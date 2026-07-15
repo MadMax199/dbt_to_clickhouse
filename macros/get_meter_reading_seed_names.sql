3c4417b951251ebb93efe5565030cf131a485409
MadMax199 <maxkrampert@web.de>
2026-07-15 22:25:33 +0200

    clickhouse docker

--- macros/get_meter_reading_seed_names.sql
+++ macros/get_meter_reading_seed_names.sql
@@ -4,12 +4,19 @@
         Ziffern besteht (z.B. "100101", "7069411"). Das sind die 1.298 Messwertdateien,
         die jeweils nach der Household_ID benannt sind. So muss die Liste nicht manuell
         gepflegt werden, wenn neue Haushalte hinzukommen oder entfernt werden.
+
+        Wichtig: graph.nodes ist während der Parse-Phase (vor der eigentlichen
+        Ausführung) noch nicht befüllt. Der execute-Guard verhindert, dass dbt
+        beim reinen Parsen (z.B. vor "dbt seed") mit "'dict object' has no
+        attribute 'nodes'" abbricht.
     -#}
     {% set seed_names = [] %}
-    {% for node in graph.nodes.values() %}
-        {% if node.resource_type == 'seed' and modules.re.match('^[0-9]+$', node.name) %}
-            {% do seed_names.append(node.name) %}
-        {% endif %}
-    {% endfor %}
+    {% if execute %}
+        {% for node in graph.nodes.values() %}
+            {% if node.resource_type == 'seed' and modules.re.match('^[0-9]+$', node.name) %}
+                {% do seed_names.append(node.name) %}
+            {% endif %}
+        {% endfor %}
+    {% endif %}
     {{ return(seed_names) }}
 {% endmacro %}
