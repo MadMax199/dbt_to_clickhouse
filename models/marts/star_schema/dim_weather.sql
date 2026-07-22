a29e5a8352930784e6d945f30f1eb3891ca7338a
MadMax199 <maxkrampert@web.de>
2026-07-22 04:03:35 +0200

    Star Schema started

---
+++ models/marts/star_schema/dim_weather.sql
@@ -0,0 +1,16 @@
+-- Dimensionstabelle: Wetter
+-
+
+select
+    weather_id,
+    weather_date,
+    temperature_max_daily,
+    temperature_min_daily,
+    temperature_avg_daily,
+    heating_degree_sia_daily,
+    heating_degree_us_daily,
+    cooling_degree_us_daily,
+    humidity_avg_daily,
+    precipitation_total_daily,
+    sunshine_duration_daily
+from {{ ref('stg_weather') }}
